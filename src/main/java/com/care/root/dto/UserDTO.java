a206d90d2f74bd79383de07c37235a6ff312308f
jinsung <tjd610@naver.com>
2021-11-22 14:58:58 +0900

    트렉젝션, aop

---
+++ src/main/java/com/care/root/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.care.root.dto;
+
+public class UserDTO {
+	private int userNum;
+
+	public int getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+}
